85b7a0dccd67a7536b9afb337cc4d6aa23f78b8d
nakanoyuki <yukich41@gmail.com>
2023-07-08 20:23:52 +0900

    動画プレーヤー追加

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -2,7 +2,8 @@ import React, { useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { fetchselectedData } from "../../apis";
 import { Store } from "../../store";
-
+import VideoPlay from "../VideoPlay/VideoPlay";
+import Linkify from "react-linkify";
 
 const VideoDetail = () => {
   const { globalState, setGlobalState } = useContext(Store);
@@ -22,7 +23,17 @@ const VideoDetail = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div>VideoDetail</div>;
+  return globalState.selected && globalState.selected.id ? (
+    <div>
+      <VideoPlay id={globalState.selected.id} />
+      <p>{globalState.selected.snippet.title}</p>
+      <hr />
+      <Linkify> <pre>{globalState.selected.snippet.description}</pre></Linkify>
+     
+    </div>
+  ) : (
+    <span>nodeta</span>
+  );
 };
 
 export default VideoDetail;
---
+++ src/components/VideoPlay/VideoPlay.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Youtube from "react-youtube"
+
+const VideoPlay = ({id}) => {
+  return (
+    <div><Youtube videoId={id}/></div>
+  )
+}
+
+export default VideoPlay
