edb71080647f4d8ba2087fc393abe6825cc609d8
nakanoyuki <yukich41@gmail.com>
2023-07-09 23:59:07 +0900

    詳細ページの最適化

--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Layout from "../components/Layout/Layout";
 import VideoDetail from "../components/VideoDetail/VideoDetail";
 import SideList from "../components/SideList/SideList";
+import { Store } from "../store";
+import { useLocation } from "react-router-dom";
+import { fetchRelatedData, fetchselectedData } from "../apis";
 
 const Watch = () => {
+  const { setGlobalState } = useContext(Store);
+  const location = useLocation();
+  const setVideos = async () => {
+    const searchParams = new URLSearchParams(location.search)
+    const id = searchParams.get("v")
+    if(id){
+      const [selected,related] = await Promise.all([fetchselectedData(id),fetchRelatedData(id)])
+      setGlobalState({type:"SET_SELECTED",payload:{selected: selected.data.items.shift()}})
+      setGlobalState({type:"SET_RELATED",payload:{related: related.data.items}})
+
+    }
+  };
+  useEffect(()=>{
+    setVideos()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[location.search])
   return (
     <Layout>
       <VideoDetail />
