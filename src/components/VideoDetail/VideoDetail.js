b65fda2dc7c55f3a32049ab267962850f2201b4c
nakanoyuki <yukich41@gmail.com>
2023-07-10 00:02:32 +0900

    子コンポーネントの不要になった記述を削除

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,35 +1,18 @@
-import React, { useContext, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { fetchselectedData } from "../../apis";
+import React, { useContext } from "react";
 import { Store } from "../../store";
 import VideoPlay from "../VideoPlay/VideoPlay";
 import Linkify from "react-linkify";
 
 const VideoDetail = () => {
-  const { globalState, setGlobalState } = useContext(Store);
-  const location = useLocation();
-
-  const setSelectedVideo = async () => {
-    const searchParams = new URLSearchParams(location.search);
-    const id = searchParams.get("v");
-    await fetchselectedData(id).then((res) => {
-      const item = res.data.items.shift();
-      setGlobalState({ type: "SET_SELECTED", payload: { selected: item } });
-    });
-  };
-
-  useEffect(() => {
-    setSelectedVideo();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location.search]);
-
+  const { globalState } = useContext(Store);
   return globalState.selected && globalState.selected.id ? (
     <div>
       <VideoPlay id={globalState.selected.id} />
       <p>{globalState.selected.snippet.title}</p>
       <hr />
-      <Linkify> <pre>{globalState.selected.snippet.description}</pre></Linkify>
-     
+      <Linkify>
+        <pre>{globalState.selected.snippet.description}</pre>
+      </Linkify>
     </div>
   ) : (
     <span>nodeta</span>
